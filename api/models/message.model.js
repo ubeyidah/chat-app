c3379913b22cc4c1ca48abc6bd033ea48edd3832
ubeyid oumer <ubeyidah@gmail.com>
2024-06-15 08:34:28 +0300

    message model

---
+++ api/models/message.model.js
@@ -0,0 +1,23 @@
+import mongoose, { mongo } from "mongoose";
+
+const messageShcema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    reciverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Message", messageShcema);
